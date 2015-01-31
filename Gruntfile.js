6e2fc78a1510da2feaad0244338b13514f2fe14e
Anders D. Johnson <anders.d.johnson.developer@gmail.com>
2015-01-31 14:02:06 -0600

    refactor, grunt, assemble, etc.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,34 +1,87 @@
 module.exports = function (grunt) {
 
   require('load-grunt-tasks')(grunt);
+  grunt.loadNpmTasks('assemble');
 
-  grunt.registerTask('default', 'less');
+  grunt.registerTask('default', ['dev']);
+
+  grunt.registerTask('dev', [
+    'assemble:dev',
+    'less:dev',
+    'copy:dev'
+  ]);
 
   grunt.initConfig({
 
     less: {
       dev: {
         options: {
-          sourceMap: true,
+          // sourceMap: true,
+          // sourceMapRootpath: './apples/',
+          // outputSourceFiles: true,
           dumpLineNumbers: 'all',
           ieCompat: true
         },
         files: [
           {
             expand: true,
-            cwd: 'less',
+            cwd: 'src/less',
             src: '**/*.less',
-            dest: 'css',
+            dest: 'public/css',
             ext: '.less.css'
           }
         ]
       }
     },
 
-    watch: {
+    assemble: {
       dev: {
-        files: 'less/**/*.less',
-        tasks: 'less:dev'
+        options: {
+          helpers: [ 'src/helpers/**/*.js' ],
+          partials: [ 'src/partials/**/*.hbs.html' ],
+          data: [ 'src/data/**/*.{json,yml}' ],
+          layout: 'layout-default.hbs.html',
+          layoutdir: 'src/layouts'
+        },
+        files: [{
+          expand: true,
+          cwd: 'src/pages',
+          src: [
+            '**/*.hbs.html'
+          ],
+          dest: 'public',
+          ext: '.html'
+        }]
+      }
+    },
+
+    copy: {
+      dev: {
+        files: [
+          {
+            expand: true,
+            cwd: 'src',
+            src: [
+              'images/**/*',
+              'js/**/*'
+            ],
+            dest: 'public'
+          }
+        ],
+        tasks: ['dev']
+      }
+    },
+
+    watch: {
+      options: {
+        livereload: true
+      },
+      dev: {
+        files: [
+          'Gruntfile.js',
+          'src/**/*'
+        ],
+        tasks: ['dev']
       }
     }
 
--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 
 * http://www.code42.com/
 * http://www.mobilize.mn/
+* https://unsplash.com/
